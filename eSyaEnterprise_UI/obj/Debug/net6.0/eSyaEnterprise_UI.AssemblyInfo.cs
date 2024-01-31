465de8acbc6506aec4e3e3cfdb85c10a347a6eeb
AbdulR <abdulr@gestalttechindia.in>
2024-01-31 12:16:21 +0530

    Age Range changes

--- eSyaEnterprise_UI/obj/Debug/net6.0/eSyaEnterprise_UI.AssemblyInfo.cs
+++ eSyaEnterprise_UI/obj/Debug/net6.0/eSyaEnterprise_UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eSyaEnterprise_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042ca475bc424dacbf551537b8b3d67f2aa77009")]
 [assembly: System.Reflection.AssemblyProductAttribute("eSyaEnterprise_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eSyaEnterprise_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
