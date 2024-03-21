6f9238d468a6bd35d372a63d263c28001b1ed410
AbdulR <abdulr@gestalttechindia.in>
2024-03-21 11:43:48 +0530

    Holiday Master model modified

--- eSyaEnterprise_UI/Areas/Admin/Models/DO_HolidayMaster.cs
+++ eSyaEnterprise_UI/Areas/Admin/Models/DO_HolidayMaster.cs
@@ -7,8 +7,10 @@ namespace eSyaEnterprise_UI.Areas.Admin.Models
     public class DO_HolidayMaster
     {
         public int BusinessKey { get; set; }
+        public int Year { get; set; }
+        public string HolidayType { get; set; }
         public DateTime HolidayDate { get; set; }
-        public string HolidayDesc { get; set; }
+        public string HolidayDesc { get; set; } 
         public bool ActiveStatus { get; set; }
         public string FormID { get; set; }
         public int UserID { get; set; }
