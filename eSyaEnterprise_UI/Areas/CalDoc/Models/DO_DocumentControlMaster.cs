55106ff0a066abda92162a0a2275b5db6d8a1357
AbdulR <abdulr@gestalttechindia.in>
2024-04-03 17:31:12 +0530

    Document control model changed

--- eSyaEnterprise_UI/Areas/CalDoc/Models/DO_DocumentControlMaster.cs
+++ eSyaEnterprise_UI/Areas/CalDoc/Models/DO_DocumentControlMaster.cs
@@ -3,10 +3,16 @@
     public class DO_DocumentControlMaster
     {
         public int DocumentId { get; set; }
-        public string DocumentDesc { get; set; } = null!;
-        public string ShortDesc { get; set; } = null!;
-        public string DocumentType { get; set; } = null!;
-        //public string? SchemaId { get; set; } = null!;
+        public string GeneLogic { get; set; }
+        public string CalendarType { get; set; }
+        public bool IsTransationMode { get; set; }
+        public bool IsStoreCode { get; set; }
+        public bool IsPaymentMode { get; set; }
+        public string SchemaId { get; set; }
+        public int ComboId { get; set; }
+        public string DocumentDesc { get; set; }
+        public string ShortDesc { get; set; }
+        public string DocumentType { get; set; }
         public bool UsageStatus { get; set; }
         public bool ActiveStatus { get; set; }
         public string FormId { get; set; }
