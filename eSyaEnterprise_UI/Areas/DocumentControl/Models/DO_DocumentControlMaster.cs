03471b06f14e124ab3c99b8479de4c3b92f7cd19
AbdulR <abdulr@gestalttechindia.in>
2024-04-27 12:33:27 +0530

    Removed un used model

--- eSyaEnterprise_UI/Areas/DocumentControl/Models/DO_DocumentControlMaster.cs
+++ eSyaEnterprise_UI/Areas/DocumentControl/Models/DO_DocumentControlMaster.cs
@@ -1,25 +1,6 @@
 ﻿namespace eSyaEnterprise_UI.Areas.DocumentControl.Models
 {
-    public class DO_DocumentControlMaster
-    {
-        public int DocumentId { get; set; }
-        public string GeneLogic { get; set; }
-        public string CalendarType { get; set; }
-        public bool IsTransationMode { get; set; }
-        public bool IsStoreCode { get; set; }
-        public bool IsPaymentMode { get; set; }
-        public string SchemaId { get; set; }
-        public int ComboId { get; set; }
-        public string DocumentDesc { get; set; }
-        public string ShortDesc { get; set; }
-        public string DocumentType { get; set; }
-        public bool UsageStatus { get; set; }
-        public bool ActiveStatus { get; set; }
-        public string FormId { get; set; }
-        public int UserID { get; set; }
-        public string TerminalID { get; set; }
-        public int Isadd { get; set; }
-    }
+   
     public class DO_DocumentControlStandard
     {
         public int FormId { get; set; }
