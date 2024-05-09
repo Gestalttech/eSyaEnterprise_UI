fa017ce244cb1e2f3b4f24514a49858fb6092c12
Siva guru <sivagurunathan@gestalttechindia.in>
2024-05-09 11:27:38 +0530

    Map patient category specialty - View,Js are added and completed

---
+++ eSyaEnterprise_UI/Areas/ConfigPatient/Controllers/SpecialtyController.cs
@@ -0,0 +1,160 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using eSyaEnterprise_UI.ApplicationCodeTypes;
+using eSyaEnterprise_UI.Areas.ConfigPatient.Data;
+using eSyaEnterprise_UI.Areas.ConfigPatient.Models;
+using eSyaEnterprise_UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace eSyaEnterprise_UI.Areas.ConfigPatient.Controllers
+{
+    [SessionTimeout]
+    public class SpecialtyController : Controller
+    {
+        private readonly IeSyaConfigPatientAPIServices _eSyaConfigPatientAPIServices;
+        private readonly ILogger<SpecialtyController> _logger;
+        public SpecialtyController(IeSyaConfigPatientAPIServices patientManagementAPIServices, ILogger<SpecialtyController> logger)
+        {
+            _eSyaConfigPatientAPIServices = patientManagementAPIServices;
+            _logger = logger;
+        }
+        #region Map Patient Category to Specialty	
+        [Area("ConfigPatient")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public async Task<IActionResult> EPM_04_00()
+        {
+
+            try
+            {
+
+                List<int> l_ac = new List<int>();
+                l_ac.Add(ApplicationCodeTypeValues.RateType);
+
+                var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<List<DO_BusinessLocation>>("CommonMethod/GetBusinessKey");
+                var ptserviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<List<DO_PatientTypCategoryAttribute>>("CommonMethod/GetActivePatientTypes");
+                if (serviceResponse.Status && ptserviceResponse.Status)
+                {
+                  
+                    ViewBag.BusinessKeyList = serviceResponse.Data.Select(a => new SelectListItem
+                    {
+
+                        Value = a.BusinessKey.ToString(),
+                        Text = a.LocationDescription.ToString()
+                    });
+                    ViewBag.PatientTypeList = ptserviceResponse.Data.Select(a => new SelectListItem
+                    {
+
+                        Value = a.PatientTypeId.ToString(),
+                        Text = a.Description.ToString()
+                    });
+                }
+                else
+                {
+                    _logger.LogError(new Exception(ptserviceResponse.Message), "UD:GetActivePatientTypes");
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetActivePatientTypes");
+                return Json(new DO_ResponseParameter() { Status = false, Message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message });
+            }
+        }
+
+
+        /// <summary>
+        ///Get Patient Types for Tree View
+        /// </summary>
+        [Produces("application/json")]
+        [HttpGet]
+        public async Task<JsonResult> GetAllPatientCategoryforTreeView(int BusinessKey)
+        {
+            try
+            {
+                List<jsTreeObject> jsTree = new List<jsTreeObject>();
+
+                jsTreeObject jsObj = new jsTreeObject
+                {
+                    id = "MM",
+                    parent = "#",
+                    text = "Patient Category",
+                    state = new stateObject { opened = true, selected = false }
+                };
+                jsTree.Add(jsObj);
+                var parameter = "?BusinessKey=" + BusinessKey;
+                var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<DO_PatientAttributes>("Specialty/GetAllPatientCategoryforTreeView" + parameter);
+                if (serviceResponse.Status)
+                {
+                    var PatientTypes = serviceResponse.Data;
+                    if (PatientTypes != null)
+                    {
+                        foreach (var f in PatientTypes.l_PatientType.OrderBy(o => o.PatientTypeId))
+                        {
+                            jsObj = new jsTreeObject
+                            {
+                                id = "MM" + f.PatientTypeId.ToString(),
+                                text = f.Description,
+                                GroupIndex = f.PatientTypeId.ToString(),
+                                parent = "MM",
+                                state = new stateObject { opened = true, selected = false }
+                            };
+                            jsTree.Add(jsObj);
+                        }
+
+                        foreach (var f in PatientTypes.l_PatienTypeCategory.OrderBy(o => o.PatientTypeId))
+                        {
+                            jsObj = new jsTreeObject
+                            {
+                                id = "SM" + f.PatientTypeId.ToString() + "_" + f.PatientCategoryId.ToString(),
+                                text = f.Description,
+                                GroupIndex = f.PatientTypeId.ToString(),
+                                parent = "MM" + f.PatientTypeId.ToString(),
+                                state = new stateObject { opened = true, selected = false }
+                            };
+                            jsTree.Add(jsObj);
+                        }
+                    }
+                    return Json(jsTree);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:GetAllPatientTypesforTreeView");
+                    return Json(new { Status = false, StatusCode = "500" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetAllPatientTypesforTreeView");
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        ///Get Patient Category Info
+        /// </summary>
+        [HttpPost]
+        public async Task<JsonResult> GetPatientCategoryInfo(int BusinessKey, int PatientTypeId, int PatientCategoryId)
+        {
+            try
+            {
+                var parameter = "?BusinessKey=" + BusinessKey+ "?PatientTypeId=" + PatientTypeId + "&PatientCategoryId=" + PatientCategoryId;
+                var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<DO_PatientTypCategoryAttribute>("Specialty/GetPatientCategoryInfo" + parameter);
+                if (serviceResponse.Status)
+                {
+                    return Json(serviceResponse.Data);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:GetPatientCategoryInfo:For PatientTypeId {0} with PatientCategoryId entered {1}", BusinessKey, PatientTypeId, PatientCategoryId);
+                    return Json(new { Status = false, StatusCode = "500" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetPatientCategoryInfo:For PatientTypeId {0} with PatientCategoryId entered {1}", BusinessKey, PatientTypeId, PatientCategoryId);
+                throw ex;
+            }
+        }
+        #endregion
+    }
+}
---
+++ eSyaEnterprise_UI/wwwroot/js/ConfigPatient/MapPatientCategorytoSpecialty.js
@@ -0,0 +1,181 @@
+﻿$(function () {
+
+});
+function fnBusinessKeyChange() {
+    var _businesskey = $("#cboBusinessKey").val();
+    var _patienttype = $("#cboPatientTypes").val();
+    if (_businesskey == 0 || _patienttype == 0) {
+        $("#jqgMapPatientCategoryDocument").jqGrid('GridUnload');
+    }
+    else {
+        fnLoadPatientTypeCategoryMapDocumentLink();
+    }
+}
+
+function fnLoadPatientCategoryTree() {
+    $.ajax({
+        url: getBaseURL() + '/Specialty/GetAllPatientCategoryforTreeView?BusinessKey=' + $("#cboBusinessKey").val(),
+        success: function (result) {
+            fnGetPatientCategory_Success(result);
+         },
+        error: function (error) {
+            fnAlert("e", "", error.StatusCode, error.statusText);
+        }
+    });
+}
+
+function fnGetPatientCategory_Success() {
+    $("#jstMapPatientCategorySpecialty").jstree({
+        "state": { "checkbox_disabled": true },
+        "checkbox": {
+            "keep_selected_style": false
+        },
+        //"plugins": ["checkbox"],
+        core: { 'data': dataArray, 'check_callback': true, 'multiple': true }
+    });
+
+    $("#jstMapPatientCategorySpecialty").on('loaded.jstree', function () {
+
+        $("#jsTreePatientType").jstree('open_all');
+        $("#jsTreePatientType").jstree()._open_to(prevSelectedID);
+        $('#jsTreePatientType').jstree().select_node(prevSelectedID);
+
+    });
+    $('#jstMapPatientCategorySpecialty').on("changed.jstree", function (e, data) {
+
+        if (data.node != undefined) {
+            if (prevSelectedID != data.node.id) {
+                prevSelectedID = data.node.id;
+
+                if (data.node.id == "0") {
+                    fnClearFields();
+                    $("#pnlMapPatientCategorySpecialty").hide();
+                }
+                else {
+
+                    $('#View').remove();
+                    $('#Edit').remove();
+                    $('#Add').remove();
+
+                    $("#pnlMapPatientCategorySpecialty").hide();
+
+                    if (data.node.parent == "MM") {
+                        $('#' + data.node.id + "_anchor").html($('#' + data.node.id + "_anchor").html() + '<span id="Add" style="padding-left:10px;padding-right:10px">&nbsp;<i class="fa fa-plus" style="color:#337ab7"aria-hidden="true"></i></span>')
+
+                        $('#Add').on('click', function () {
+                            if (_userFormRole.IsInsert === false) {
+                                $('#pnlMapPatientCategorySpecialty').hide();
+                                fnAlert("w", "EPM_04_00", "UIC01", errorMsg.addauth_E1);
+                                return;
+                            }
+                            $("#pnlMapPatientCategorySpecialty").show();
+                            $(".mdl-card__title-text").text(localization.AddPatientCategory);
+                            fnClearFields();
+
+
+                            $('#txtPatientTypeId').val(data.node.id.substring(2));
+
+                            $("#txtPatientCategoryId").val('');
+
+                            $("input[id*='chk']").attr('disabled', false);
+                            //Enable category dropdown and check boxes
+                            $("#cboPatientcategory").prop('disabled', false).selectpicker("refresh");
+                            Isactivestatus = false;
+                            isinsert = true;
+                            $("#btnAddMapPatientCategorySpecialty").show();
+                            $("input,textarea").attr('readonly', false);
+                            $("select").next().attr('disabled', false);
+                            $("#btnAddMapPatientCategorySpecialty").html('<i class="fa fa-plus"></i> ' + localization.Save);
+                            $("#chkActiveStatus").parent().addClass("is-checked");
+                            $("#chkActiveStatus").attr('disabled', true);
+                            $("input[type=checkbox]").attr('disabled', false);
+                        });
+                    }
+                    else if (data.node.id.startsWith("SM")) {
+
+                        NodeID = 0;
+                        NodeID = data.node.id.substring(2).split("_")[1];
+
+                        $('#' + data.node.id + "_anchor").html($('#' + data.node.id + "_anchor").html() + '<span id="View" style="padding-left:10px">&nbsp;<i class="fa fa-eye" style="color:#337ab7"aria-hidden="true"></i></span>')
+                        $('#' + data.node.id + "_anchor").html($('#' + data.node.id + "_anchor").html() + '<span id="Edit" style="padding-left:10px">&nbsp;<i class="fa fa-pen" style="color:#337ab7"aria-hidden="true"></i></span>')
+
+                        $('#View').on('click', function () {
+
+                            if (_userFormRole.IsView === false) {
+                                $('#pnlMapPatientCategorySpecialty').hide();
+                                fnAlert("w", "EPM_04_00", "UIC03", errorMsg.vieweauth_E3);
+                                return;
+                            }
+
+                            $("#pnlMapPatientCategorySpecialty").show();
+                            $(".mdl-card__title-text").text(localization.ViewPatientCategory);
+                            $('#txtPatientCategoryId').val(NodeID);
+                            $('#txtPatientTypeId').val(data.node.id.substring(2).split("_")[0]);
+
+                            fnFillPatientCategoryInfo();
+
+                            //disable the check boxes
+                            Isactivestatus = true;
+                            isinsert = false;
+                            $("#btnAddMapPatientCategorySpecialty").hide();
+                            $("input,textarea").attr('readonly', true);
+                            $("select").next().attr('disabled', true);
+                            $("input[type=checkbox]").attr('disabled', true);
+                        });
+
+                        $('#Edit').on('click', function () {
+
+                            if (_userFormRole.IsEdit === false) {
+                                $('#pnlMapPatientCategorySpecialty').hide();
+                                fnAlert("w", "EPM_04_00", "UIC02", errorMsg.editauth_E2);
+                                return;
+                            }
+
+                            $("#pnlMapPatientCategorySpecialty").show();
+                            $(".mdl-card__title-text").text(localization.EditPatientCategory);
+                            $('#txtPatientCategoryId').val(NodeID);
+                            $('#txtPatientTypeId').val(data.node.id.substring(2).split("_")[0]);
+
+                            fnFillPatientCategoryInfo();
+
+                            //enableing check boxes
+                            Isactivestatus = false;
+                            isinsert = false;
+                            //disable category dropdown
+                            $("#cboPatientcategory").next().attr('disabled', true).selectpicker("refresh");
+                            //$("#cboRateType").next().attr('disabled', false).selectpicker("refresh");
+
+                            $("#btnAddMapPatientCategorySpecialty").show();
+                            $("input,textarea").attr('readonly', false);
+                            //$("select").next().attr('disabled', false);
+                            $("input[type=checkbox]").attr('disabled', false);
+                            $("#btnAddMapPatientCategorySpecialty").html('<i class="fa fa-sync"></i> ' + localization.Update);
+                        });
+
+
+                    }
+                    else {
+                        fnClearFields();
+                        $("#pnlMapPatientCategorySpecialty").hide();
+                    }
+                }
+            }
+        }
+    });
+}
+
+
+function fnClearFields() {
+    $("#cboBusinessKey").val('0').selectpicker('refresh');
+    $("#cboPatientTypes").val('0').selectpicker('refresh');
+}
+
+function fnExpandAll() {
+    $('#jstMapPatientCategorySpecialty').jstree('open_all');
+}
+
+function fnCollapseAll() {
+    fnClearFields();
+    $("#pnlMapPatientCategorySpecialty").hide();
+    $('#jstMapPatientCategorySpecialty').jstree('close_all');
+}
