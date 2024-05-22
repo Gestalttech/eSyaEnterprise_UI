85c1f1886883a1b7fc5c3283741507ff86c8739c
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-05-22 17:13:12 +0530

    ESP_01_00, Manage Doctor and Leave management  controller, view js added.

---
+++ eSyaEnterprise_UI/Areas/ServiceProvider/Controllers/DoctorController.cs
@@ -0,0 +1,101 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.ServiceProvider.Controllers
+{
+    [SessionTimeout]
+    public class DoctorController : Controller
+    {
+
+        #region Manage Doctor
+        [Area("ServiceProvider")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult ESP_01_00()
+        {
+            return View();
+        }
+
+        #region Doctor Profile
+        [Area("ServiceProvider")]
+        public IActionResult _DoctorProfile()
+        {
+            return View();
+        }
+        #endregion
+
+        #region About Doctor
+        [Area("ServiceProvider")]
+        public IActionResult _AboutDoctor()
+        {
+            return View();
+        }
+        #endregion
+
+
+        #region PhotoAndSignature
+        [Area("ServiceProvider")]
+        public IActionResult _PhotoAndSignature()
+        {
+            return View();
+        }
+        #endregion
+
+
+        #region Address
+
+        [Area("ServiceProvider")]
+        public IActionResult _Address()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Statutory Details
+        [Area("ServiceProvider")]
+        public IActionResult _StatutoryDetails()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Business Link
+        [Area("ServiceProvider")]
+        public IActionResult _BusinessLink()
+        {
+            return View();
+        }
+        #endregion
+
+
+        #region Specialty Link
+        [Area("ServiceProvider")]
+        public IActionResult _SpecialtyLink()
+        {
+            return View();
+
+        }
+        #endregion
+
+        #region ClinicLink
+        [Area("ServiceProvider")]
+        public IActionResult _ClinicLink()
+        {
+            return View();
+
+        }
+        #endregion
+
+
+        #region ConsultationRates
+        [Area("ServiceProvider")]
+        public IActionResult _ConsultationRates()
+        {
+            return View();
+
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ eSyaEnterprise_UI/Areas/ServiceProvider/Controllers/LeaveController.cs
@@ -0,0 +1,19 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.ServiceProvider.Controllers
+{
+    [SessionTimeout]
+    public class LeaveController : Controller
+    {
+
+        #region Doctor Leave
+        [Area("ServiceProvider")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult ESP_02_00()
+        {
+            return View();
+        }
+        #endregion region
+    }
+}
