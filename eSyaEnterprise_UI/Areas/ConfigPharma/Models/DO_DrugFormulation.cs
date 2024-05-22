5b03a4561bce13798c4ebd2c8664a529e2d8e481
AbdulR <abdulr@gestalttechindia.in>
2024-05-22 17:52:43 +0530

    Drug Formulation  Model created

---
+++ eSyaEnterprise_UI/Areas/ConfigPharma/Models/DO_DrugFormulation.cs
@@ -0,0 +1,19 @@
+﻿namespace eSyaEnterprise_UI.Areas.ConfigPharma.Models
+{
+    public class DO_DrugFormulation
+    {
+        public int CompositionId { get; set; }
+        public int FormulationId { get; set; }
+        public string FormulationDesc { get; set; } = null!;
+        public int DrugForm { get; set; }
+        public string? Volume { get; set; }
+        public int MethodOfAdministration { get; set; }
+        public bool ActiveStatus { get; set; }
+        public string FormID { get; set; }
+        public int UserID { get; set; }
+        public string TerminalID { get; set; }
+        public List<DO_eSyaParameter> lst_formulationparams { get; set; }
+        public string? DrugFormDesc { get; set; }
+        public string? MethodOfAdministrationDesc { get; set; }
+    }
+}
