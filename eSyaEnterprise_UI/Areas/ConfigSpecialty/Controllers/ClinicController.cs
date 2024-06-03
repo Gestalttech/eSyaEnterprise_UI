1504b3ca7064b1798e8003706d066b0a1dc28de6
AbdulR <abdulr@gestalttechindia.in>
2024-06-03 14:09:24 +0530

    Map Specialty Clinic Consultation Link

--- eSyaEnterprise_UI/Areas/ConfigSpecialty/Controllers/ClinicController.cs
+++ eSyaEnterprise_UI/Areas/ConfigSpecialty/Controllers/ClinicController.cs
@@ -4,6 +4,8 @@ using Newtonsoft.Json;
 using eSyaEnterprise_UI.Utility;
 using eSyaEnterprise_UI.ActionFilter;
 using eSyaEnterprise_UI.Areas.ConfigSpecialty.Data;
+using eSyaEnterprise_UI.Areas.ConfigSpecialty.Models;
+using eSyaEnterprise_UI.Models;
 
 namespace eSyaEnterprise_UI.Areas.ConfigSpecialty.Controllers
 {
@@ -25,6 +27,139 @@ namespace eSyaEnterprise_UI.Areas.ConfigSpecialty.Controllers
         public IActionResult ECP_07_00()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Get Specialty List From Specialty Business Link
+        /// </summary>
+
+        [HttpPost]
+        public async Task<JsonResult> GetMappedSpecialtyListbyBusinessKey(int businessKey)
+        {
+            try
+            {
+                var response = await _eSyaConfigSpecialtyAPIServices.HttpClientServices.GetAsync<List<DO_SpecialtyBusinessLink>>("DoctorMaster/GetMappedSpecialtyListbyBusinessKey?businessKey=" + businessKey);
+                return Json(response.Data);
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new DO_ReturnParameter() { Status = false, Message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message });
+            }
+        }
+
+        [Produces("application/json")]
+        [HttpGet]
+        public async Task<JsonResult> GetMapedSpecialtyClinicConsultationTypeBySpecialtyID(int businessKey, int specialtyId)
+        {
+            try
+            {
+                List<jsTreeObject> ClinicConsultantTree = new List<jsTreeObject>();
+
+                jsTreeObject jsObj = new jsTreeObject
+                {
+                    id = "CL0",
+                    parent = "#",
+                    text = "Clinics",
+                    //icon = "/images/jsTree/foldergroupicon.png",
+                    state = new stateObject { opened = true, selected = false, checkbox_disabled = true, disabled = true }
+                };
+                ClinicConsultantTree.Add(jsObj);
+
+                var clinicResponse = await _eSyaConfigSpecialtyAPIServices.HttpClientServices.GetAsync<List<DO_MapSpecialtyClinicConsultationType>>("DoctorMaster/GetDoctorClinicLinkList?businessKey=" + businessKey + "&specialtyId=" + specialtyId + "&doctorId=" + doctorId);
+                if (clinicResponse.Status)
+                {
+                    if (clinicResponse.Data != null)
+                    {
+                        List<DO_MapSpecialtyClinicConsultationType> clinic_list = clinicResponse.Data.Select(x => new DO_MapSpecialtyClinicConsultationType { ConsultationId = x.ConsultationId, ConsultationDesc = x.ConsultationDesc }).GroupBy(y => y.ConsultationId, (key, grp) => grp.FirstOrDefault()).ToList();
+                        foreach (DO_MapSpecialtyClinicConsultationType cl in clinic_list)
+                        {
+                            jsObj = new jsTreeObject
+                            {
+                                id = cl.ConsultationId.ToString(),
+                                text = cl.ConsultationDesc.ToString(),
+                                //icon = "/images/jsTree/openfolder.png",
+                                parent = "CL0",
+                                state = new stateObject { opened = true, selected = false, checkbox_disabled = true, disabled = true },
+                            };
+                            ClinicConsultantTree.Add(jsObj);
+                        }
+
+                        var conusltant_list = clinicResponse.Data;
+                        foreach (var co in conusltant_list.Where(w => conusltant_list.Any(f => f.ConsultationId == w.ConsultationId)))
+                        {
+                            jsObj = new jsTreeObject
+                            {
+                                text = co.ClinicDesc,
+                                parent = co.ConsultationId.ToString()
+
+                            };
+                            if (co.BusinessKey > 0)
+                            {
+                                jsObj.id = co.ConsultationId.ToString() + "_" + "Y" + "_" + co.ClinicId;
+                                //jsObj.icon = "/images/jsTree/checkedstate.jpg";
+                                if (co.ActiveStatus)
+                                    jsObj.state = new stateObject { opened = true, selected = true, Checked = true };
+                                else
+                                    jsObj.state = new stateObject { opened = true, selected = false, Checked = false };
+                            }
+                            else
+                            {
+                                jsObj.id = co.ConsultationId.ToString() + "_" + "N" + "_" + co.ClinicId;
+                                //jsObj.icon = "/images/jsTree/fileIcon.png";
+                                jsObj.state = new stateObject { opened = true, selected = false, Checked = false };
+                            }
+
+                            ClinicConsultantTree.Add(jsObj);
+                        }
+                    }
+
+                    return Json(ClinicConsultantTree);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(clinicResponse.Message), "UD:GetMapedSpecialtyClinicConsultationTypeBySpecialtyID:For BusinessId {0} , SpecialtyId {1} and DoctorId entered {2}", businessKey, specialtyId, doctorId);
+                    return Json(new { Status = false, StatusCode = "500" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetMapedSpecialtyClinicConsultationTypeBySpecialtyID:For BusinessId {0} , SpecialtyId {1} and DoctorId entered {2}", businessKey, specialtyId, doctorId);
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Insert / Update Doctor Clinic
+        /// </summary>
+        [HttpPost]
+        public async Task<JsonResult> InsertUpdateDoctorClinicLink(List<DO_MapSpecialtyClinicConsultationType> do_cl)
+        {
+            try
+            {
+                do_cl.All(c =>
+                {
+                    c.UserID = AppSessionVariables.GetSessionUserID(HttpContext);
+                    c.TerminalID = AppSessionVariables.GetIPAddress(HttpContext);
+                    c.FormId = AppSessionVariables.GetSessionFormInternalID(HttpContext);
+                    return true;
+                });
+
+                var Insertresponse = await _eSyaConfigSpecialtyAPIServices.HttpClientServices.PostAsJsonAsync<DO_ReturnParameter>("DoctorMaster/DO_MapSpecialtyClinicConsultationType", do_cl);
+                if (Insertresponse.Status)
+                    return Json(Insertresponse.Data);
+                else
+                {
+                    _logger.LogError(new Exception(Insertresponse.Message), "UD:InsertUpdateDoctorClinicLink:params:" + JsonConvert.SerializeObject(do_cl));
+                    return Json(new DO_ReturnParameter() { Status = false, Message = Insertresponse.Message });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:DO_MapSpecialtyClinicConsultationType:params:" + JsonConvert.SerializeObject(do_cl));
+                throw ex;
+            }
         }
         #endregion
     }
