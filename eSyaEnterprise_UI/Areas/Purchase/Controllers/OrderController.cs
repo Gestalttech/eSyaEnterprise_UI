dc0597e041a1e6d22c383bb33ed105ff40975b30
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-06-22 17:30:05 +0530

    Added Purchase area, Terms, order controller, EPO_01_00, EPO_02_00 view and js files.

---
+++ eSyaEnterprise_UI/Areas/Purchase/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.Purchase.Controllers
+{
+    [SessionTimeout]
+    public class OrderController : Controller
+    {
+        [Area("Purchase")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult EPO_02_00()
+        {
+            return View();
+        }
+    }
+}
---
+++ eSyaEnterprise_UI/Areas/Purchase/Controllers/TermsController.cs
@@ -0,0 +1,16 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.Purchase.Controllers
+{
+    [SessionTimeout]
+    public class TermsController : Controller
+    {
+        [Area("Purchase")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult EPO_01_00()
+        {
+            return View();
+        }
+    }
+}
