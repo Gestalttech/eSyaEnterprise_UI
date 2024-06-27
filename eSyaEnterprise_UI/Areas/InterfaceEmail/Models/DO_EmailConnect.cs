775aab05186dadae2f41738a320d24849b97e9d6
AbdulR <abdulr@gestalttechindia.in>
2024-06-27 18:02:05 +0530

    user Id changed to User Name

--- eSyaEnterprise_UI/Areas/InterfaceEmail/Models/DO_EmailConnect.cs
+++ eSyaEnterprise_UI/Areas/InterfaceEmail/Models/DO_EmailConnect.cs
@@ -7,7 +7,7 @@
         public int Port { get; set; }
         public int EmailType { get; set; }
         public string SenderEmailId { get; set; }
-        public string UserId { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public string? PassKey { get; set; }
         public bool ActiveStatus { get; set; }
