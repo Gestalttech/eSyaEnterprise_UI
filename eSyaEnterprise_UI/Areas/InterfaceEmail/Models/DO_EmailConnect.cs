afecd5003ba4464ccf75d34b439cf2e94afdb3e9
AbdulR <abdulr@gestalttechindia.in>
2024-06-27 16:57:37 +0530

    Email template model modified

--- eSyaEnterprise_UI/Areas/InterfaceEmail/Models/DO_EmailConnect.cs
+++ eSyaEnterprise_UI/Areas/InterfaceEmail/Models/DO_EmailConnect.cs
@@ -5,11 +5,17 @@
         public int BusinessKey { get; set; }
         public string OutgoingMailServer { get; set; }
         public int Port { get; set; }
+        public int EmailType { get; set; }
+        public string SenderEmailId { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public string? PassKey { get; set; }
         public bool ActiveStatus { get; set; }
         public string FormID { get; set; }
         public int UserID { get; set; }
         public string TerminalID { get; set; }
         public int ISDCode { get; set; }
         public bool status { get; set; }
+        public string? EmailTypeDesc { get; set; }
     }
 }
