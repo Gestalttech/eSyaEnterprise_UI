e849257bfe8d828fc86ee486c5c3ecbb36cb2dea
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-07-03 12:17:44 +0530

    Book type added view function

--- eSyaEnterprise_UI/Areas/ConfigFin/Controllers/BookTypeController.cs
+++ eSyaEnterprise_UI/Areas/ConfigFin/Controllers/BookTypeController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eSyaEnterprise_UI.Areas.ConfigFin.Controllers
 {
+    [SessionTimeout]
     public class BookTypeController : Controller
     {
+        [Area("ConfigFin")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
         public IActionResult EAC_01_00()
         {
             return View();
