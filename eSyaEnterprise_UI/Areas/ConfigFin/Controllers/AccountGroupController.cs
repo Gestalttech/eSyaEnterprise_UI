f7a54f3e74ba1c8b1dee72c13de7e1ef9e7c29b3
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-07-05 16:35:57 +0530

    AccountGroupController

--- eSyaEnterprise_UI/Areas/ConfigFin/Controllers/AccountGroupController.cs
+++ eSyaEnterprise_UI/Areas/ConfigFin/Controllers/AccountGroupController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eSyaEnterprise_UI.Areas.ConfigFin.Controllers
 {
+    [SessionTimeout]
     public class AccountGroupController : Controller
     {
+        [Area("ConfigFin")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
         public IActionResult EAC_03_00()
         {
             return View();
