b54232b3a925eac259ee18fcb98eb2034e2b6d6f
AbdulR <abdulr@gestalttechindia.in>
2024-07-08 11:17:47 +0530

    Model Added

---
+++ eSyaEnterprise_UI/Areas/ConfigFAsset/Models/DO_DepreciationMethod.cs
@@ -0,0 +1,21 @@
+﻿namespace eSyaEnterprise_UI.Areas.ConfigFAsset.Models
+{
+    public class DO_DepreciationMethod
+    {
+        public int Isdcode { get; set; }
+        public int AssetGroup { get; set; }
+        public int AssetSubGroup { get; set; }
+        public DateTime EffectiveFrom { get; set; }
+        public int DepreciationMethod { get; set; }
+        public decimal DepreciationPercentage { get; set; }
+        public int UsefulYears { get; set; }
+        public DateTime? EffectiveTill { get; set; }
+        public bool ActiveStatus { get; set; }
+        public int AssetGroupDesc { get; set; }
+        public int AssetSubGroupDesc { get; set; }
+        public string? DepreciationMethodDesc { get; set; }
+        public string FormID { get; set; }
+        public int UserID { get; set; }
+        public string TerminalID { get; set; }
+    }
+}
