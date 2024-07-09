abaac19fc01220892ae35151afd118a22661f932
AbdulR <abdulr@gestalttechindia.in>
2024-07-09 13:02:23 +0530

    App file

---
+++ eSyaEnterprise_UI/Areas/ConfigFAsset/Models/DO_ApplicationCodes.cs
@@ -0,0 +1,23 @@
+﻿namespace eSyaEnterprise_UI.Areas.ConfigFAsset.Models
+{
+    public class DO_ApplicationCodes
+    {
+        public int ApplicationCode { get; set; }
+        public int CodeType { get; set; }
+        public string CodeDesc { get; set; }
+    }
+
+    public class DO_BusinessLocation
+    {
+        public int BusinessKey { get; set; }
+        public string LocationDescription { get; set; }
+    }
+    public class DO_ISDCodes
+    {
+        public int Isdcode { get; set; }
+        public string CountryName { get; set; }
+        public string CountryFlag { get; set; }
+        public string CountryCode { get; set; }
+
+    }
+}
