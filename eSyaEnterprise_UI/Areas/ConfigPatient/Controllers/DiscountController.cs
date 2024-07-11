3f78e6f2d2ad5d60b6d118115f0f1e849df6c569
AbdulR <abdulr@gestalttechindia.in>
2024-07-11 11:09:07 +0530

    Creating Consuming API configuration in UI

--- eSyaEnterprise_UI/Areas/ConfigPatient/Controllers/DiscountController.cs
+++ eSyaEnterprise_UI/Areas/ConfigPatient/Controllers/DiscountController.cs
@@ -7,6 +7,7 @@ using eSyaEnterprise_UI.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using eSyaEnterprise_UI.ApplicationCodeTypes;
+using Newtonsoft.Json;
 
 namespace eSyaEnterprise_UI.Areas.ConfigPatient.Controllers
 {
@@ -28,25 +29,32 @@ namespace eSyaEnterprise_UI.Areas.ConfigPatient.Controllers
         {
             try
             {
+                List<int> l_codeType = new List<int>();
+                l_codeType.Add(ApplicationCodeTypeValues.DiscountFor);
+                l_codeType.Add(ApplicationCodeTypeValues.DiscountAt);
+
                 var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<List<DO_BusinessLocation>>("CommonMethod/GetBusinessKey");
-                var DiscountForResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<List<DO_ApplicationCodes>>("CommonData/GetApplicationCodesByCodeType?codeType=" + ApplicationCodeTypeValues.DiscountFor);
-                var DiscountAtResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<List<DO_ApplicationCodes>>("CommonData/GetApplicationCodesByCodeType?codeType=" + ApplicationCodeTypeValues.DiscountAt);
-               
-                if (serviceResponse.Status && DiscountForResponse.Status && DiscountAtResponse.Status)
+                var appresponse = await _eSyaConfigPatientAPIServices.HttpClientServices.PostAsJsonAsync<List<DO_ApplicationCodes>>("CommonMethod/GetApplicationCodesByCodeTypeList", l_codeType);
+
+
+                if (serviceResponse.Status && appresponse.Status)
                 {
+                    List<DO_ApplicationCodes> DiscountFor = appresponse.Data.Where(x => x.CodeType == ApplicationCodeTypeValues.DiscountFor).ToList();
+                    List<DO_ApplicationCodes> DiscountAt = appresponse.Data.Where(x => x.CodeType == ApplicationCodeTypeValues.DiscountAt).ToList();
+
                     ViewBag.BusinessKeyList = serviceResponse.Data.Select(a => new SelectListItem
                     {
                         Value = a.BusinessKey.ToString(),
                         Text = a.LocationDescription.ToString()
                     });
 
-                    ViewBag.DiscountForList = DiscountForResponse.Data.Select(b => new SelectListItem
+                    ViewBag.DiscountForList = DiscountFor.Select(b => new SelectListItem
                     {
                         Value = b.ApplicationCode.ToString(),
                         Text = b.CodeDesc,
                     }).ToList();
 
-                    ViewBag.DiscountAtList = DiscountForResponse.Data.Select(c => new SelectListItem
+                    ViewBag.DiscountAtList = DiscountAt.Select(c => new SelectListItem
                     {
                         Value = c.ApplicationCode.ToString(),
                         Text = c.CodeDesc,
@@ -61,8 +69,162 @@ namespace eSyaEnterprise_UI.Areas.ConfigPatient.Controllers
             }
         }
 
-               
+        [HttpGet]
+        public async Task<JsonResult> GetActivePatientCategoriesbyBusinessKey(int businesskey)
+        {
+            try
+            {
+                var parameter = "?businesskey=" + businesskey;
+                var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<List<DO_PatientCategoryDiscount>>("Discount/GetActivePatientCategoriesbyBusinessKey" + parameter);
+                if (serviceResponse.Status)
+                {
+                    return Json(serviceResponse.Data);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:GetActivePatientCategoriesbyBusinessKey:For businesskey {0} ", businesskey);
+                    return Json(new { Status = false, StatusCode = "500" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetActivePatientCategoriesbyBusinessKey:For businesskey {0} ", businesskey);
+                throw ex;
+            }
         }
-        #endregion
+        [HttpGet]
+        public async Task<JsonResult> GetPatientCategoryDiscountbyDiscountAt(int businesskey, int patientcategoryId, int discountfor, bool serviceclass)
+        {
+            try
+            {
+                var parameter = "?businesskey=" + businesskey + "&patientcategoryId=" + patientcategoryId + "&discountfor=" + discountfor
+                    + "&serviceclass=" + serviceclass;
+                var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.GetAsync<List<DO_PatientCategoryDiscount>>("Discount/GetPatientCategoryDiscountbyDiscountAt" + parameter);
+                if (serviceResponse.Status)
+                {
+                    return Json(serviceResponse.Data);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:GetPatientCategoryDiscountbyDiscountAt:For businesskey {0} ", businesskey);
+                    return Json(new { Status = false, StatusCode = "500" });
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetPatientCategoryDiscountbyDiscountAt:For businesskey {0} ", businesskey);
+                throw ex;
+            }
+        }
+        [HttpPost]
+        public async Task<JsonResult> InsertOrUpdatePatientCategoryDiscount(bool isinsert, DO_PatientCategoryDiscount obj)
+        {
+            try
+            {
+                if (obj.DiscountAt == 650001)
+                {
+                    obj.serviceclass = true;
+                }
+                else
+                {
+                    obj.serviceclass = false;
+                }
+                    obj.FormID = AppSessionVariables.GetSessionFormInternalID(HttpContext);
+                    obj.UserID = AppSessionVariables.GetSessionUserID(HttpContext);
+                    obj.TerminalID = AppSessionVariables.GetIPAddress(HttpContext);
+                    if (isinsert)
+                    {
+                        var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.PostAsJsonAsync<DO_ReturnParameter>("Discount/InsertPatientCategoryDiscount", obj);
+                        if (serviceResponse.Status)
+                            return Json(serviceResponse.Data);
+                        else
+                        {
+                            _logger.LogError(new Exception(serviceResponse.Message), "UD:InsertPatientCategoryDiscount:params:" + JsonConvert.SerializeObject(obj));
+                            return Json(new DO_ReturnParameter() { Status = false, Message = serviceResponse.Message });
+                        }
+                    }
+                    else
+                    {
+                        var serviceResponse = await _eSyaConfigPatientAPIServices.HttpClientServices.PostAsJsonAsync<DO_ReturnParameter>("Discount/UpdatePatientCategoryDiscount", obj);
+                        if (serviceResponse.Status)
+                            return Json(serviceResponse.Data);
+                        else
+                        {
+                            _logger.LogError(new Exception(serviceResponse.Message), "UD:UpdatePatientCategoryDiscount:params:" + JsonConvert.SerializeObject(obj));
+                            return Json(new DO_ReturnParameter() { Status = false, Message = serviceResponse.Message });
+                        }
+                    }
+
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:InsertOrUpdatePatientCategoryDiscount:params:" + JsonConvert.SerializeObject(obj));
+                return Json(new { Status = false, Message = ex.ToString() });
+            }
+        }
+        [HttpPost]
+        public JsonResult GetPatientPatientCategoryDiscountInfo(DO_PatientCategoryDiscount obj)
+        {
+            try
+            {
+
+              
+                    obj.UserID = AppSessionVariables.GetSessionUserID(HttpContext);
+                    obj.TerminalID = AppSessionVariables.GetIPAddress(HttpContext);
+                    obj.FormID = AppSessionVariables.GetSessionFormInternalID(HttpContext);
+                
+
+                var Insertresponse = _eSyaConfigPatientAPIServices.HttpClientServices.PostAsJsonAsync<DO_ReturnParameter>("Discount/GetPatientPatientCategoryDiscountInfo", obj).Result;
+                if (Insertresponse.Status)
+                {
+                    return Json(Insertresponse.Data);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(Insertresponse.Message), "UD:GetPatientPatientCategoryDiscountInfo:Params:" + JsonConvert.SerializeObject(obj));
+                    return Json(new DO_ReturnParameter() { Status = false, Message = Insertresponse.Message });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetPatientPatientCategoryDiscountInfo:Params:" + JsonConvert.SerializeObject(obj));
+                return Json(new DO_ReturnParameter() { Status = false, Message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult ActiveOrDeActivePatientCategoryDiscount(bool status, DO_PatientCategoryDiscount obj)
+        {
+            try
+            {
+
+
+                obj.UserID = AppSessionVariables.GetSessionUserID(HttpContext);
+                obj.TerminalID = AppSessionVariables.GetIPAddress(HttpContext);
+                obj.FormID = AppSessionVariables.GetSessionFormInternalID(HttpContext);
+
+
+                var Insertresponse = _eSyaConfigPatientAPIServices.HttpClientServices.PostAsJsonAsync<DO_ReturnParameter>("Discount/ActiveOrDeActivePatientCategoryDiscount", obj).Result;
+                if (Insertresponse.Status)
+                {
+                    return Json(Insertresponse.Data);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(Insertresponse.Message), "UD:ActiveOrDeActivePatientCategoryDiscount:Params:" + JsonConvert.SerializeObject(obj));
+                    return Json(new DO_ReturnParameter() { Status = false, Message = Insertresponse.Message });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:ActiveOrDeActivePatientCategoryDiscount:Params:" + JsonConvert.SerializeObject(obj));
+                return Json(new DO_ReturnParameter() { Status = false, Message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message });
+            }
+        }
     }
+    #endregion
+}
 
---
+++ eSyaEnterprise_UI/Areas/ConfigPatient/Models/DO_PatientCategoryDiscount.cs
@@ -0,0 +1,23 @@
+﻿namespace eSyaEnterprise_UI.Areas.ConfigPatient.Models
+{
+    public class DO_PatientCategoryDiscount
+    {
+        public int BusinessKey { get; set; }
+        public int PatientCategoryId { get; set; }
+        public int DiscountFor { get; set; }
+        public int DiscountAt { get; set; }
+        public int ServiceClassId { get; set; }
+        public decimal ServiceChargePerc { get; set; }
+        public string DiscountRule { get; set; } = null!;
+        public decimal DiscountPerc { get; set; }
+        public bool ActiveStatus { get; set; }
+        public int ServiceId { get; set; }
+        public string FormID { get; set; }
+        public int UserID { get; set; }
+        public string TerminalID { get; set; }
+        public List<DO_eSyaParameter> l_discountparams { get; set; }
+        public string? PatientCategoryDesc { get; set; }
+        public string? ServiceClassDesc { get; set; }
+        public bool serviceclass { get; set; }
+    }
+}
