549fe848bc793a81a059c438c96de67e8fced0ff
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-07-16 17:08:17 +0530

    created GWayrules - EPS_20_00

---
+++ eSyaEnterprise_UI/Areas/ProductSetup/Controllers/GWayRulesController.cs
@@ -0,0 +1,16 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.ProductSetup.Controllers
+{
+    [SessionTimeout]
+    public class GWayRulesController : Controller
+    {
+        [Area("ProductSetup")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult EPS_20_00()
+        {
+            return View();
+        }
+    }
+}
