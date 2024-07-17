9af6f6d64bb083e158e80fcecdeb6a0bed9ff399
AbdulR <abdulr@gestalttechindia.in>
2024-07-17 11:57:37 +0530

    Gateway Rules model created

---
+++ eSyaEnterprise_UI/Areas/ProductSetup/Models/DO_GatewayRules.cs
@@ -0,0 +1,13 @@
+﻿namespace eSyaEnterprise_UI.Areas.ProductSetup.Models
+{
+    public class DO_GatewayRules
+    {
+        public int GwruleId { get; set; }
+        public string Gwdesc { get; set; }
+        public int RuleValue { get; set; }
+        public bool ActiveStatus { get; set; }
+        public string FormID { get; set; }
+        public int UserID { get; set; }
+        public string TerminalID { get; set; }
+    }
+}
