215b5c5c875e27b1e1b2fb1d4fb2d906f3b74dfa
AbdulR <abdulr@gestalttechindia.in>
2024-08-02 12:46:21 +0530

    GST Model created

---
+++ eSyaEnterprise_UI/Areas/ConfigPharma/Models/DO_GST.cs
@@ -0,0 +1,14 @@
+﻿namespace eSyaEnterprise_UI.Areas.ConfigPharma.Models
+{
+    public class DO_GST
+    {
+        public decimal Hsncode { get; set; }
+        public decimal Gstperc { get; set; }
+        public DateTime EffectiveFrom { get; set; }
+        public DateTime? EffectiveTill { get; set; }
+        public bool ActiveStatus { get; set; }
+        public string FormID { get; set; }
+        public int UserID { get; set; }
+        public string TerminalID { get; set; }
+    }
+}
