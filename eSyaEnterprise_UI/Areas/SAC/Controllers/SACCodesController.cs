1c3bf7dba678a394a1d97d570df3e61abe43fd66
AbdulR <abdulr@gestalttechindia.in>
2024-08-06 16:13:36 +0530

    SAC Code

--- eSyaEnterprise_UI/Areas/SAC/Controllers/SACCodesController.cs
+++ eSyaEnterprise_UI/Areas/SAC/Controllers/SACCodesController.cs
@@ -32,123 +32,251 @@ namespace eSyaEnterprise_UI.Areas.SAC.Controllers
             return View();
         }
 
-        //public async Task<ActionResult> GetSACCodes(int ISDCode)
-        //{
-        //    try
-        //    {
-        //        var serviceResponse = await _eSyaSACAPIServices.HttpClientServices.GetAsync<List<DO_SACClass>>("SACClass/GetSACClasses");
-        //        var st_list = new List<DO_SACClass>();
-        //        if (serviceResponse.Status)
-        //        {
-        //            st_list = serviceResponse.Data.FindAll(w => w.ActiveStatus == true);
-        //        }
-        //        else
-        //        {
-        //            _logger.LogError(new Exception(serviceResponse.Message), "UD:GetSACClasses:GetSACClasses");
-        //        }
-        //        var serviceResponse1 = await _eSyaSACAPIServices.HttpClientServices.GetAsync<List<DO_SACCategory>>("SACCategory/GetSACCategories");
-        //        var sg_list = new List<DO_SACCategory>();
-        //        if (serviceResponse1.Status)
-        //        {
-        //            sg_list = serviceResponse1.Data.FindAll(w => w.ActiveStatus == true);
-        //        }
-        //        else
-        //        {
-        //            _logger.LogError(new Exception(serviceResponse1.Message), "UD:GetSACCategories:GetSACCategories");
-        //        }
-        //        var sc_list = new List<DO_SACCodes>();
-        //        var serviceResponse2 = await _eSyaSACAPIServices.HttpClientServices.GetAsync<List<DO_SACCodes>>("SACCodes/GetSACCodes");
-        //        if (serviceResponse2.Status)
-        //        {
-        //            sc_list = serviceResponse2.Data;
-        //        }
-        //        else
-        //        {
-        //            _logger.LogError(new Exception(serviceResponse2.Message), "UD:GetSACCodes:GetSACCodes");
-        //        }
-        //        List<jsTreeObject> treeView = new List<jsTreeObject>();
-        //        jsTreeObject jsObj = new jsTreeObject();
+        public async Task<ActionResult> GetSACCodes(int ISDCode)
+        {
+            try
+            {
+                var parameter = "?ISDCode=" + ISDCode;
+                var serviceResponse = await _eSyaSACAPIServices.HttpClientServices.GetAsync<List<DO_SACClass>>("SACClass/GetSACClasses"+ parameter);
+                var st_list = new List<DO_SACClass>();
+                if (serviceResponse.Status)
+                {
+                    st_list = serviceResponse.Data.FindAll(w => w.ActiveStatus == true);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:GetSACClasses:GetSACClasses");
+                }
+                var serviceResponse1 = await _eSyaSACAPIServices.HttpClientServices.GetAsync<List<DO_SACCategory>>("SACCategory/GetSACCategories"+ parameter);
+                var sg_list = new List<DO_SACCategory>();
+                if (serviceResponse1.Status)
+                {
+                    sg_list = serviceResponse1.Data.FindAll(w => w.ActiveStatus == true);
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse1.Message), "UD:GetSACCategories:GetSACCategories");
+                }
+                var sc_list = new List<DO_SACCodes>();
+                var serviceResponse2 = await _eSyaSACAPIServices.HttpClientServices.GetAsync<List<DO_SACCodes>>("SACCodes/GetSACCodes"+ parameter);
+                if (serviceResponse2.Status)
+                {
+                    sc_list = serviceResponse2.Data;
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse2.Message), "UD:GetSACCodes:GetSACCodes");
+                }
+                List<jsTreeObject> treeView = new List<jsTreeObject>();
+                jsTreeObject jsObj = new jsTreeObject();
 
-        //        string baseURL = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
+                string baseURL = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
 
-        //        jsObj.id = "SC";
-        //        jsObj.parent = "#";
-        //        jsObj.text = "Service Classes";
-        //        jsObj.icon = "/images/jsTree/foldergroupicon.png";
-        //        jsObj.state = new stateObject { opened = true, selected = false };
-        //        treeView.Add(jsObj);
-        //        if (st_list != null)
-        //        {
-        //            foreach (var st in st_list)
-        //            {
-        //                jsObj = new jsTreeObject();
-        //                jsObj.id = "T" + st.Sacclass.ToString();
-        //                jsObj.text = st.SacclassDesc;
-        //                jsObj.icon = baseURL + "/images/jsTree/openfolder.png";
-        //                jsObj.parent = "SC";
-        //                jsObj.state = new stateObject { opened = false, selected = false };
-        //                treeView.Add(jsObj);
-        //                if (sg_list != null)
-        //                {
-        //                    foreach (var sg in sg_list)
-        //                    {
-        //                        if (st.Sacclass == sg.Sacclass)
-        //                        {
-        //                            jsObj = new jsTreeObject();
-        //                            jsObj.id = sg.Saccategory.ToString();
-        //                            jsObj.text = sg.SaccategoryDesc;
-        //                            jsObj.icon = baseURL + "/images/jsTree/openfolder.png";
-        //                            jsObj.parent = "T" + st.Sacclass.ToString();
-        //                            jsObj.state = new stateObject { opened = false, selected = false };
-        //                            treeView.Add(jsObj);
-
-
-        //                            if (sc_list != null)
-        //                            {
-        //                                foreach (var sc in sc_list)
-        //                                {
-        //                                    if (sg.Saccategory == sc.Saccategory)
-        //                                    {
-        //                                        if (sc.ServiceClassId == sc.ParentId)
-        //                                        {
-        //                                            jsObj = new jsTreeObject();
-        //                                            jsObj.id = "C" + sc.ServiceClassId.ToString();
-        //                                            jsObj.text = sc.ServiceClassDesc;
-        //                                            jsObj.icon = baseURL + "/images/jsTree/fileIcon.png";
-        //                                            jsObj.parent = sg.ServiceGroupId.ToString();
-        //                                            treeView.Add(jsObj);
-        //                                        }
-        //                                        else
-        //                                        {
-        //                                            jsObj = new jsTreeObject();
-        //                                            jsObj.id = "C" + sc.ServiceClassId.ToString();
-        //                                            jsObj.text = sc.ServiceClassDesc;
-        //                                            jsObj.icon = baseURL + "/images/jsTree/fileIcon.png";
-        //                                            jsObj.parent = "C" + sc.ParentId.ToString();
-        //                                            treeView.Add(jsObj);
-        //                                        }
-
-        //                                    }
-        //                                }
-        //                            }
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        }
-
-        //        return Json(treeView);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError(ex, "UD:GetServiceClasses");
-        //        return Json(new DO_ReturnParameter() { Status = false, Message = ex.ToString() });
-        //    }
+                jsObj.id = "SC";
+                jsObj.parent = "#";
+                jsObj.text = "Service Classes";
+                jsObj.icon = "/images/jsTree/foldergroupicon.png";
+                jsObj.state = new stateObject { opened = true, selected = false };
+                treeView.Add(jsObj);
+                if (st_list != null)
+                {
+                    foreach (var st in st_list)
+                    {
+                        jsObj = new jsTreeObject();
+                        jsObj.id = "T" + st.Sacclass.ToString();
+                        jsObj.text = st.SacclassDesc;
+                        jsObj.icon = baseURL + "/images/jsTree/openfolder.png";
+                        jsObj.parent = "SC";
+                        jsObj.state = new stateObject { opened = false, selected = false };
+                        treeView.Add(jsObj);
+                        if (sg_list != null)
+                        {
+                            foreach (var sg in sg_list)
+                            {
+                                if (st.Sacclass == sg.Sacclass)
+                                {
+                                    jsObj = new jsTreeObject();
+                                    jsObj.id = sg.Saccategory.ToString();
+                                    jsObj.text = sg.SaccategoryDesc;
+                                    jsObj.icon = baseURL + "/images/jsTree/openfolder.png";
+                                    jsObj.parent = "T" + st.Sacclass.ToString();
+                                    jsObj.state = new stateObject { opened = false, selected = false };
+                                    treeView.Add(jsObj);
 
 
+                                    if (sc_list != null)
+                                    {
+                                        foreach (var sc in sc_list)
+                                        {
+                                            if (sg.Saccategory == sc.Saccategory)
+                                            {
+                                                if (sc.Saccode == sc.ParentId)
+                                                {
+                                                    jsObj = new jsTreeObject();
+                                                    jsObj.id = "C" + sc.Saccode.ToString();
+                                                    jsObj.text = sc.Sacdescription;
+                                                    jsObj.icon = baseURL + "/images/jsTree/fileIcon.png";
+                                                    jsObj.parent = sg.Saccategory.ToString();
+                                                    treeView.Add(jsObj);
+                                                }
+                                                else
+                                                {
+                                                    jsObj = new jsTreeObject();
+                                                    jsObj.id = "C" + sc.Saccode.ToString();
+                                                    jsObj.text = sc.Sacdescription;
+                                                    jsObj.icon = baseURL + "/images/jsTree/fileIcon.png";
+                                                    jsObj.parent = "C" + sc.ParentId.ToString();
+                                                    treeView.Add(jsObj);
+                                                }
 
-        //}
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
 
+                return Json(treeView);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetServiceClasses");
+                return Json(new DO_ReturnParameter() { Status = false, Message = ex.ToString() });
+            }
+
+
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetSACCodeByCode(int ISDCode, string SACClassID, string SACCategoryID, string SACCodeID)
+        {
+            try
+            {
+                var parameter = "?ISDCode=" + ISDCode + "&SACClassID=" + SACClassID + "&SACCategoryID=" + SACCategoryID + "&SACCodeID=" + SACCodeID;
+                var serviceResponse = await _eSyaSACAPIServices.HttpClientServices.GetAsync<DO_SACCodes>("SACCodes/GetSACCodeByCode" + parameter);
+                if (serviceResponse.Status)
+                {
+                    if (serviceResponse.Data != null)
+                    {
+                        var data = serviceResponse.Data;
+                        return Json(data);
+                    }
+                    else
+                    {
+                        _logger.LogError(new Exception(serviceResponse.Message), "UD:GetSACCodeByCode:For SACCodeID {0}", SACCodeID);
+                        return Json(new { Status = false, Message = serviceResponse.Message });
+                    }
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:GetSACCodeByCode:For SACCodeID {0}", SACCodeID);
+                    return Json(new { Status = false, Message = serviceResponse.Message });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetSACCodeByCode:For SACCodeID {0}", SACCodeID);
+                return Json(new { Status = false, Message = ex.ToString() });
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> InsertOrUpdateSACCode(bool _isInsert, DO_SACCodes obj)
+        {
+            try
+            {
+                obj.FormID = AppSessionVariables.GetSessionFormInternalID(HttpContext);
+                obj.UserID = AppSessionVariables.GetSessionUserID(HttpContext);
+                obj.TerminalID = AppSessionVariables.GetIPAddress(HttpContext);
+                if (_isInsert)
+                {
+                    var serviceResponse = await _eSyaSACAPIServices.HttpClientServices.PostAsJsonAsync<DO_ReturnParameter>("SACCode/InsertIntoSACCode", obj);
+                    if (serviceResponse.Status)
+                    {
+                        if (serviceResponse.Data != null)
+                        {
+                            return Json(serviceResponse.Data);
+                        }
+                        else
+                        {
+                            _logger.LogError(new Exception(serviceResponse.Data.Message), "UD:InsertIntoSACCode:params:" + JsonConvert.SerializeObject(obj));
+                            return Json(new DO_ReturnParameter() { Status = false, Message = serviceResponse.Data.Message });
+                        }
+
+                    }
+                    else
+                    {
+                        _logger.LogError(new Exception(serviceResponse.Message), "UD:InsertIntoSACCategory:params:" + JsonConvert.SerializeObject(obj));
+                        return Json(new DO_ReturnParameter() { Status = false, Message = serviceResponse.Message });
+                    }
+                }
+                else
+                {
+                    var serviceResponse = await _eSyaSACAPIServices.HttpClientServices.PostAsJsonAsync<DO_ReturnParameter>("SACCode/UpdateSACSACCode", obj);
+                    if (serviceResponse.Status)
+                    {
+                        if (serviceResponse.Data != null)
+                        {
+                            return Json(serviceResponse.Data);
+                        }
+                        else
+                        {
+                            _logger.LogError(new Exception(serviceResponse.Data.Message), "UD:UpdateSACSACCode:params:" + JsonConvert.SerializeObject(obj));
+                            return Json(new DO_ReturnParameter() { Status = false, Message = serviceResponse.Data.Message });
+                        }
+
+                    }
+                    else
+                    {
+                        _logger.LogError(new Exception(serviceResponse.Message), "UD:InsertOrUpdateSACCode:params:" + JsonConvert.SerializeObject(obj));
+                        return Json(new DO_ReturnParameter() { Status = false, Message = serviceResponse.Message });
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:InsertOrUpdateSACCategory:params:" + JsonConvert.SerializeObject(obj));
+                return Json(new DO_ReturnParameter() { Status = false, Message = ex.ToString() });
+            }
+        }
+        public async Task<ActionResult> DeleteSACCode(int ISDCode, string SACClassID, string SACCategoryID, string SACCodeID)
+        {
+            try
+            {
+                var parameter = "?ISDCode=" + ISDCode + "&SACClassID=" + SACClassID + "&SACCategoryID=" + SACCategoryID
+                    + "&SACCodeID=" + SACCodeID;
+
+                var serviceResponse = await _eSyaSACAPIServices.HttpClientServices.GetAsync<DO_ReturnParameter>("SACCode/DeleteSACCode" + parameter);
+                if (serviceResponse.Status)
+                {
+                    if (serviceResponse.Data != null)
+                    {
+                        return Json(serviceResponse.Data);
+                    }
+                    else
+                    {
+                        _logger.LogError(new Exception(serviceResponse.Data.Message), "UD:DeleteSACCode:For SACCode {0} ", SACCodeID);
+                        return Json(new { Status = false, Message = serviceResponse.Data.Message });
+                    }
+
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:DeleteSACCode:For SACCode {0}", SACCodeID);
+                    return Json(new DO_ReturnParameter() { Status = false, Message = serviceResponse.Message });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:DeleteSACCode:For SACCode {0} ", SACCodeID);
+                return Json(new { Status = false, Message = ex.ToString() });
+            }
+        }
         #endregion
     }
 }
--- eSyaEnterprise_UI/Areas/SAC/Models/DO_SACCodes.cs
+++ eSyaEnterprise_UI/Areas/SAC/Models/DO_SACCodes.cs
@@ -5,13 +5,12 @@
         public int Isdcode { get; set; }
         public string Sacclass { get; set; }
         public string Saccategory { get; set; }
-        //public int ParentId { get; set; }
         public string Saccode { get; set; }
         public string Sacdescription { get; set; }
         public bool ActiveStatus { get; set; }
         public string FormID { get; set; }
         public int UserID { get; set; }
         public string TerminalID { get; set; }
-
+        public string ParentId { get; set; }
     }
 }
