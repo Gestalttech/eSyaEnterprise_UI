b8e40d7d6dd91bb29fe488bf10161cbce1dc389f
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-08-06 18:09:54 +0530

    Tax identification model

--- eSyaEnterprise_UI/Areas/ProductSetup/Models/DO_TaxIdentification.cs
+++ eSyaEnterprise_UI/Areas/ProductSetup/Models/DO_TaxIdentification.cs
@@ -14,6 +14,8 @@ namespace eSyaEnterprise_UI.Areas.ProductSetup.Models
         public string TaxIdentificationDesc { get; set; }
         public string StateCode { get; set; }
         public int InsertStatus { get; set; }
+
+        public bool IsUt { get; set; }
         public bool ActiveStatus { get; set; }
         public string FormId { get; set; }
         public int UserID { get; set; }
