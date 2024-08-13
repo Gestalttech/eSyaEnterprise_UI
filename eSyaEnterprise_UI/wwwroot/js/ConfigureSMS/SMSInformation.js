0acce9962b0a27597fe0d5741f78ef5d5366428d
Mohideen <86177238+MohideenGtpl@users.noreply.github.com>
2024-08-13 11:54:23 +0530

    Commit SMS Parameter

--- eSyaEnterprise_UI/wwwroot/js/ConfigureSMS/SMSInformation.js
+++ eSyaEnterprise_UI/wwwroot/js/ConfigureSMS/SMSInformation.js
@@ -61,13 +61,14 @@ function fnGridLoadSMSEmptyParameter() {
         datatype: 'local',
         ajaxGridOptions: { contentType: 'application/json; charset=utf-8' },
         jsonReader: { repeatitems: false, root: "rows", page: "page", total: "total", records: "records" },
-        colNames: [localization.SMSID, localization.SMSDescription, localization.IsVariable, localization.TriggeringEvent, localization.SMSStatement, localization.Active, localization.Actions],
+        colNames: [localization.SMSID, localization.SMSDescription, localization.IsVariable, localization.TriggeringEvent, localization.TriggeringEvent, localization.SMSStatement, localization.Active, localization.Actions],
         colModel: [
             { name: "Smsid", width: 70, editable: true, align: 'left' },
             { name: "Smsdescription", width: 70, editable: false, align: 'left', resizable: true },
             { name: "IsVariable", width: 35, editable: true, align: 'center', resizable: false, edittype: "checkbox", formatter: 'checkbox', editoptions: { value: "true:false" }, formatoptions: { disabled: true } },
             { name: "Tevent", width: 70, align: 'center', resizable: false, editoption: { 'text-align': 'left', maxlength: 25 }, hidden: true },
-            { name: "Smsstatement", width: 150, align: 'left', resizable: false, editoption: { 'text-align': 'left', maxlength: 250 } },
+            { name: "TEventDesc", width: 70, align: 'left', resizable: false, editoption: { 'text-align': 'left', maxlength: 250 } },
+            { name: "Smsstatement", width: 150, align: 'left', resizable: false, editoption: { 'text-align': 'left', maxlength: 250 }, hidden: true },
             { name: "ActiveStatus", editable: true, width: 48, align: 'center', resizable: false, edittype: "checkbox", formatter: 'checkbox', editoptions: { value: "true:false" }, formatoptions: { disabled: true } },
             {
                 name: 'edit', search: false, align: 'left', width: 35, sortable: false, resizable: false,
@@ -116,13 +117,14 @@ function fnGridLoadSMSParameter() {
         mtype: 'Post',
         ajaxGridOptions: { contentType: 'application/json; charset=utf-8' },
         jsonReader: { repeatitems: false, root: "rows", page: "page", total: "total", records: "records" },
-        colNames: [localization.SMSID, localization.SMSDescription, localization.IsVariable, localization.TriggeringEvent, localization.SMSStatement, localization.Active, localization.Actions],
+        colNames: [localization.SMSID, localization.SMSDescription, localization.IsVariable, localization.TriggeringEvent, localization.TriggeringEvent, localization.SMSStatement, localization.Active, localization.Actions],
         colModel: [
             { name: "Smsid", width: 70, editable: true, align: 'left' },
             { name: "Smsdescription", width: 70, editable: false, align: 'left', resizable: true },
             { name: "IsVariable", width: 35, editable: true, align: 'center', resizable: false, edittype: "checkbox", formatter: 'checkbox', editoptions: { value: "true:false" }, formatoptions: { disabled: true } },
             { name: "Tevent", width: 70, align: 'center', resizable: false, editoption: { 'text-align': 'left', maxlength: 25 }, hidden: true },
-            { name: "Smsstatement", width: 150, align: 'left', resizable: false, editoption: { 'text-align': 'left', maxlength: 250 } },
+            { name: "TEventDesc", width: 70, align: 'left', resizable: false, editoption: { 'text-align': 'left', maxlength: 250 } },
+            { name: "Smsstatement", width: 150, align: 'left', resizable: false, editoption: { 'text-align': 'left', maxlength: 250 }, hidden: true },
             { name: "ActiveStatus", editable: true, width: 48, align: 'center', resizable: false, edittype: "checkbox", formatter: 'checkbox', editoptions: { value: "true:false" }, formatoptions: { disabled: true } },
            {
                 name: 'edit', search: false, align: 'left', width: 35, sortable: false, resizable: false,
