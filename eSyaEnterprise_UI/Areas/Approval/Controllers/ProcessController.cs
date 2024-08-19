d6c85c7c2688564c06f5f1220b310fcf4dd90609
AbdulR <abdulr@gestalttechindia.in>
2024-08-19 18:50:48 +0530

    Bind location dynamically

--- eSyaEnterprise_UI/Areas/Approval/Controllers/ProcessController.cs
+++ eSyaEnterprise_UI/Areas/Approval/Controllers/ProcessController.cs
@@ -1,17 +1,52 @@
 ﻿using eSyaEnterprise_UI.ActionFilter;
+using eSyaEnterprise_UI.Areas.Approval.Data;
+using eSyaEnterprise_UI.Areas.Approval.Models;
+using eSyaEnterprise_UI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace eSyaEnterprise_UI.Areas.Approval.Controllers
 {
     [SessionTimeout]
     public class ProcessController : Controller
     {
+        private readonly IeSyaApprovalProcessAPIServices _eSyaApprovalProcessAPIServices;
+        private readonly ILogger<ProcessController> _logger;
+
+        public ProcessController(IeSyaApprovalProcessAPIServices eSyaApprovalProcessAPIServices, ILogger<ProcessController> logger)
+        {
+            _eSyaApprovalProcessAPIServices = eSyaApprovalProcessAPIServices;
+            _logger = logger;
+        }
         #region Define Approval Process
         [Area("Approval")]
         [ServiceFilter(typeof(ViewBagActionFilter))]
-        public IActionResult EAP_01_00()
+        public async Task<IActionResult> EAP_01_00()
         {
-            return View();
+            try
+            {
+                var serviceResponse = await _eSyaApprovalProcessAPIServices.HttpClientServices.GetAsync<List<DO_BusinessLocation>>("CommonData/GetBusinessKey");
+                if (serviceResponse.Status)
+                {
+                    ViewBag.BusinessKeyList = serviceResponse.Data.Select(b => new SelectListItem
+                    {
+                        Value = b.BusinessKey.ToString(),
+                        Text = b.LocationDescription,
+                    }).ToList();
+
+                    return View();
+                }
+                else
+                {
+                    _logger.LogError(new Exception(serviceResponse.Message), "UD:GetBusinessKey");
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UD:GetBusinessKey");
+                return Json(new DO_ReturnParameter() { Status = false, Message = (ex.InnerException != null) ? ex.InnerException.Message : ex.Message });
+            }
         }
         
 
---
+++ eSyaEnterprise_UI/Areas/Approval/Models/DO_BusinessLocation.cs
@@ -0,0 +1,8 @@
+﻿namespace eSyaEnterprise_UI.Areas.Approval.Models
+{
+    public class DO_BusinessLocation
+    {
+        public int BusinessKey { get; set; }
+        public string LocationDescription { get; set; }
+    }
+}
