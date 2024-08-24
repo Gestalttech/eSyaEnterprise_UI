ed2d2cd3535b88e20400bd8d7aa556f657e02d0f
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-08-24 11:40:38 +0530

    EFA-04_00 - ManageChartOfAccounts - Controller,View,JS created.

---
+++ eSyaEnterprise_UI/Areas/FinAdmin/Controllers/COAController.cs
@@ -0,0 +1,16 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.FinAdmin.Controllers
+{
+    [SessionTimeout]
+    public class COAController : Controller
+    {
+        [Area("FinAdmin")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult EFA_04_00()
+        {
+            return View();
+        }
+    }
+}
