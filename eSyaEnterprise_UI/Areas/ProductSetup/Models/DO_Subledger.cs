e1248d6f7b330bcfcb434de435974956ec7eb94a
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-08-29 15:55:23 +0530

    DO_Subledger - Model file added.

---
+++ eSyaEnterprise_UI/Areas/ProductSetup/Models/DO_Subledger.cs
@@ -0,0 +1,15 @@
+﻿namespace eSyaEnterprise_UI.Areas.ProductSetup.Models
+{
+    public class DO_Subledger
+    {
+        public string SubledgerType { get; set; }
+        public string? Sltdesc { get; set; }
+        public int SubledgerGroup { get; set; }
+        public string? SubledgerDesc { get; set; }
+        public string? Coahead { get; set; }
+        public bool ActiveStatus { get; set; }
+        public int UserID { get; set; }
+        public string FormID { get; set; }
+        public string TerminalID { get; set; }
+    }
+}
