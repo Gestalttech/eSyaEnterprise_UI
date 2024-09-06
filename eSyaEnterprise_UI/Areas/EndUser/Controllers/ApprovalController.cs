3393e91f1bad221909590d4e17d4e9793e8f340f
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-09-06 13:32:37 +0530

    EEU_12_00 - Map User to Approvals - View, JS, controller created.

---
+++ eSyaEnterprise_UI/Areas/EndUser/Controllers/ApprovalController.cs
@@ -0,0 +1,18 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.EndUser.Controllers
+{
+    [SessionTimeout]
+    public class ApprovalController : Controller
+    {
+        #region Map User to Approvals
+        [Area("EndUser")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult EEU_12_00()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
