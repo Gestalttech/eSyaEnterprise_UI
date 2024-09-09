6660b2ccf62b00f89a2bd1948ac4fc6c1f891c32
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-09-09 18:50:18 +0530

    added view,js,controller for eap_02_00

---
+++ eSyaEnterprise_UI/Areas/ViewAdmin/Controllers/ApproverController.cs
@@ -0,0 +1,16 @@
+﻿using eSyaEnterprise_UI.ActionFilter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSyaEnterprise_UI.Areas.ViewAdmin.Controllers
+{
+    [SessionTimeout]
+    public class ApproverController : Controller
+    {
+        [Area("ViewAdmin")]
+        [ServiceFilter(typeof(ViewBagActionFilter))]
+        public IActionResult EAP_02_00()
+        {
+            return View();
+        }
+    }
+}
