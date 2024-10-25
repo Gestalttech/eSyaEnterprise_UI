896780dd66dd09b700b69112bc8f8029b4a07795
Sivaguru <sivagurunathan@gestalttechindia.in>
2024-10-25 10:47:17 +0530

    Message notification js

--- eSyaEnterprise_UI/wwwroot/js/Common/eSyaMessageNotification.js
+++ eSyaEnterprise_UI/wwwroot/js/Common/eSyaMessageNotification.js
@@ -21,33 +21,21 @@
 
 function fnAlert(_messageType, _messageTitle, _messageCode, _messageContent) {
     var _msgtype = "";
-    debugger;
     var _culture = localStorage.getItem("culture");
+    var _msgtypecapitalize = "";
 
     if (_culture == "ar-EG") {
         (_msgtype = "w") ? _msgtypecapitalize = "تحذير" : ((_msgtype = "e") ? _msgtypecapitalize = "خطأ" : _msgtypecapitalize = "نجاح");
-        //$(".er-bg-warning").val("تحذير");
-        //$(".er-bg-error").val("خطأ");      
-        //$(".er-bg-success").val("نجاح");      
-    }
+     }
     if (_culture == "hi-IN") {
         (_msgtype = "w") ? _msgtypecapitalize = "चेतावनी" : ((_msgtype = "e") ? _msgtypecapitalize = "गलती" : _msgtypecapitalize = "सफलता");
-        //$(".er-bg-warning").val("चेतावनी");
-        //$(".er-bg-error").val("गलती");
-        //$(".er-bg-success").val("सफलता");
     }
     if (_culture == "en-US") {
         (_msgtype = "w") ? _msgtypecapitalize = "Warning" : ((_msgtype = "e") ? _msgtypecapitalize = "Error" : _msgtypecapitalize = "Success");
- 
-        //$(".er-bg-warning").val("Warning");
-        //$(".er-bg-error").val("Error");
-        //$(".er-bg-success").val("Success");
-        
-    }
+     }
         _msgtype = (_messageType == "w") ? "warning" : (_messageType == "e") ? "error" : "success";
   
     
-    //var _msgtypecapitalize = (_msgtype[0].toUpperCase() + _msgtype.slice(1));
     var _msgtitle = (_messageTitle == "") ? "" : " - " + _messageTitle;
     var _msgcode = (_messageCode == "") ? "" : _messageCode;
 
